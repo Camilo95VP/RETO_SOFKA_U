78de4c8cc4f37f38f1b8e47d0983e32c72b5aad7
JuanC <camiloflstudio@gmail.com>
2022-04-26 19:48:13 -0500

    Clase para ingresar jugador

---
+++ src/main/java/Game/IngresarJugador.java
@@ -0,0 +1,34 @@
+
+package Game;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class IngresarJugador {
+    String nombre;
+    int puntaje;
+    public IngresarJugador(String nombre,int puntaje) throws IOException{
+        this.nombre=nombre;
+        this.puntaje=puntaje;
+        
+        String fichJugador="Registro Jugadores";
+        File ficheroJ=new File(fichJugador);
+    
+        if(ficheroJ.exists()==false){
+            ficheroJ.mkdirs();
+        } 
+        
+        BufferedWriter docJugador;
+        docJugador=new BufferedWriter(new FileWriter("Registro Jugadores/"+nombre+".csv"));
+                                                
+        docJugador.write("Jugador,Puntaje\n");
+        docJugador.write(nombre+","+puntaje);
+        docJugador.close();
+        System.out.println("Jugador creado");     
+    }   
+    
+}
+
