1fd7d4bf7dbfab2a6a8b7dbee3857c5618099643
JuanC <camiloflstudio@gmail.com>
2022-04-27 19:26:48 -0500

    ingresando valores por consola

--- src/main/java/Game/GameQuestions.java
+++ src/main/java/Game/GameQuestions.java
@@ -205,4 +205,64 @@ public class GameQuestions {
                 }
                     
             
+            
+if(opcion.equals("2")){
+
+                System.out.println("Que categoria deseas para la pregunta: 1,2,3,4 ó 5"); //ingresando valores por consola
+                Scanner opc=new Scanner(System.in);
+                String opcionCategoria=opc.nextLine();
+
+                System.out.println("Escribe la pregunta: ");
+                String pregunta=opc.nextLine();
+                
+                System.out.println("A continuacion ingrese las cuatro opciones de respuesta: ");
+
+                System.out.println("Opción 1 de respuesta");
+                String opcion1=opc.nextLine();
+
+                System.out.println("Opción 2 de respuesta");
+                String opcion2=opc.nextLine();
+
+                System.out.println("Opción 3 de respuesta");
+                String opcion3=opc.nextLine();
+
+                System.out.println("Opción 4 de respuesta");
+                String opcion4=opc.nextLine();
+
+                System.out.println("Ingrese cual de las cuantro opciones es la respuesta correcta: 1, 2, 3 ó 4");
+                String respuesta=opc.nextLine();
+
+                IngresarPregunta preguntaCreada=new IngresarPregunta(opcionCategoria,pregunta,opcion1,opcion2,opcion3,opcion4,respuesta);
+
             }
+            if(opcion.equals("3")){
+                System.out.println("Nombre de jugador: ");
+                Scanner nom=new Scanner(System.in);
+                nombre=nom.nextLine();
+                
+                String fichJugador="Registro Jugadores/"+nombre+".csv";
+                File ficheroJ=new File(fichJugador);
+                if(ficheroJ.exists()==false){
+                    IngresarJugador jugador=new IngresarJugador(nombre,0);
+                }    
+
+                else {
+                    
+                
+                    BufferedReader docJugador;
+                    docJugador=new BufferedReader(new FileReader("Registro Jugadores/"+nombre+".csv"));
+                                                
+                    String info = docJugador.readLine();
+                    String info2 = docJugador.readLine();
+                    String [] listaOpciones = info2.split(",");
+                    
+                    puntaje=Integer.parseInt(listaOpciones[1]);
+                    puntajeAcumulado=puntaje;
+                    docJugador.close();
+                }
+
+            }
+        }
+    }
+  }
+}
